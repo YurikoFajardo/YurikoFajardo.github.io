498259df71e9d08789bf487e2f5256131e553f70
Yuriko Fajardo <lluridia64@hotmail.com>
2024-06-05 22:15:27 -0600

    cam

---
+++ slides.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${(i - index) * 100}%)`;
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Cambia la imagen cada 3 segundos
+
+// Inicializar el slider
+showSlide(currentSlide);
